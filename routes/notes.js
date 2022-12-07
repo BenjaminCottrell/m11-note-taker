2d2da604382565d140862d27d89c674ebe78ccf7
BenCottrell <bencottrell48@yahoo.com>
2022-12-06 23:45:37 -0500

    added a delete route for notes

--- routes/notes.js
+++ routes/notes.js
@@ -29,6 +29,22 @@ notes.post('/', (req, res) => {
     }
   });     
 
-
+// Delete route for a note
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Create a new array containing all notes except the note that matches the id in the route
+        const result = json.filter((note) => note.id != noteId);
   
+        // Save the array back to the file system
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Your note ${noteId} is now deleted`);
+      });
+  });
+
+// export notes to be required in index  
 modules.exports = notes;
